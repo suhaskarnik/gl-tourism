7e4ae0d10504764a9ead8fcd1026a6529b933409
Suhas Karnik <suhaskarnik@users.noreply.github.com>
2025-11-15 08:39:15 +0530

    registration pipeline

---
+++ tourism_project/model_building/data_register.py
@@ -0,0 +1,24 @@
+
+from huggingface_hub import create_repo, HfApi
+from huggingface_hub.utils import RepositoryNotFoundError, HfHubHTTPError
+import os
+
+api = HfApi(token=os.getenv("HF_TOKEN"))
+
+data_repo = "sam-vimes/tourism_data"
+repo_type = "dataset"
+data_dir = "tourism_project/data"
+
+# Step 1: Check if the space exists
+try:
+    api.repo_info(repo_id=data_repo, repo_type=repo_type)
+    print(f"Space '{data_repo}' already exists. Using it.")
+except RepositoryNotFoundError:
+    print(f"Space '{data_repo}' not found. Creating new space...")
+    create_repo(repo_id=data_repo, repo_type=repo_type, private=False)
+    print(f"Space '{data_repo}' created.")
+
+api.upload_folder(folder_path=data_dir, repo_id=data_repo, repo_type="dataset")
+
+
+# api.create_repo(data_repo, token=hf_token, repo_type="dataset", exist_ok=True)
